02b6c1929c11273576936a037445b33832a7d776
IuliiaSaprykina <iuliia.proskurnina@gmail.com>
2020-10-16 17:55:20 -0400

    Final version before deploy

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,34 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import{ init } from 'emailjs-com';
+
+
+init("user_7MmNAMDrOD4YCzlRmohr1");
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
+
+// var data = {
+//   service_id: 'YOUR_SERVICE_ID',
+//   template_id: 'YOUR_TEMPLATE_ID',
+//   user_id: 'YOUR_USER_ID',
+//   template_params: {
+//       'username': 'James',
+//       'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...'
+//   }
+// };
+
 export class ContactsComponent implements OnInit {
 
-  messageForm: FormGroup;
-  submitted = false;
-  success = false;
-
-  constructor(private formBuilder: FormBuilder) { 
-    this.messageForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      message: ['', Validators.required],
-      email: ['', [Validators.compose([Validators.required, Validators.email])]],
-    })
-  }
-
-  onSubmit() {
-    this.submitted = true;
-
-    if (this.messageForm.invalid) {
-      return;
-    }
-
-    this.success = true;
-  }
+  
+  
 
   ngOnInit() {
   }
